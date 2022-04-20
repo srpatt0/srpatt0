7f9e8a7bb09f52d53caf9111d485a777e563fae4
srpatt0 <69892160+srpatt0@users.noreply.github.com>
2022-04-20 23:00:37 +0530

    Add files via upload

---
+++ StockAPP/src/main/java/com/iiht/StockMarket/model/CompanyDetails.java
@@ -0,0 +1,110 @@
+package com.iiht.StockMarket.model;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import javax.persistence.Id;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name="CompanyDetails",uniqueConstraints = {@UniqueConstraint(columnNames = "companyCode")})
+public class CompanyDetails implements Serializable {
+
+	private static final long serialVersionUID = 531050392622113165L;
+
+	@Id
+	//@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "companyCode",unique = true)
+	private Long companyCode;
+	private String stockExchange;
+	private String companyName;
+	private String companyCEO;
+	private Double turnover;
+    private String boardOfDirectors;
+  	private String companyProfile;
+		
+	//---------------------------------------------------------------------------------------------------------------------------------
+	public CompanyDetails() {
+		super();
+	}
+	public CompanyDetails(Long companyCode, String stockExchange, String companyName, String companyCEO, Double turnover, String boardOfDirectors, String companyProfile) {
+		super();
+		this.companyCode = companyCode;
+		this.stockExchange = stockExchange;
+		this.companyName = companyName;
+		this.companyCEO = companyCEO;
+		this.turnover = turnover;
+		this.boardOfDirectors = boardOfDirectors;
+		this.companyProfile = companyProfile;
+	}
+
+	//---------------------------------------------------------------------------------------------------------------------------------
+	public Long getCompanyCode() {
+		return companyCode;
+	}
+	public void setCompanyCode(Long companyCode) {
+		this.companyCode = companyCode;
+	}
+	//---------------------------------------------------------------------------------------------------------------------------------
+	public String getStockExchange() {
+		return stockExchange;
+	}
+	public void setStockExchange(String stockExchange) {
+		this.stockExchange = stockExchange;
+	}
+	//---------------------------------------------------------------------------------------------------------------------------------
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	//---------------------------------------------------------------------------------------------------------------------------------
+	public String getCompanyCEO() {
+		return companyCEO;
+	}
+	public void setCompanyCEO(String companyCEO) {
+		this.companyCEO = companyCEO;
+	}
+	//---------------------------------------------------------------------------------------------------------------------------------
+	public Double getTurnover() {
+		return turnover;
+	}
+	public void setTurnover(Double turnover) {
+		this.turnover = turnover;
+	}
+	//---------------------------------------------------------------------------------------------------------------------------------
+	public String getBoardOfDirectors() {
+		return boardOfDirectors;
+	}
+	public void setBoardOfDirectors(String boardOfDirectors) {
+        //this.boardOfDirectors = "";
+        this.boardOfDirectors = boardOfDirectors;
+	}
+	//---------------------------------------------------------------------------------------------------------------------------------
+	public String getCompanyProfile() {
+		return companyProfile;
+	}
+	public void setCompanyProfile(String companyProfile) {
+		this.companyProfile = companyProfile;
+	}
+	//---------------------------------------------------------------------------------------------------------------------------------
+	public Set<StockPriceDetails> getStockPriceDetails() {
+		return StockPriceDetails;
+	}
+	public void setStockPriceDetails(Set<StockPriceDetails> stockPriceDetails) {
+		StockPriceDetails = stockPriceDetails;
+	}
+	//---------------------------------------------------------------------------------------------------------------------------------
+	@OneToMany(cascade={CascadeType.MERGE})
+	@JoinColumn(name="companyCode")
+	private Set<StockPriceDetails> StockPriceDetails;
+}
---
+++ StockAPP/src/main/java/com/iiht/StockMarket/model/StockPriceDetails.java
@@ -0,0 +1,94 @@
+package com.iiht.StockMarket.model;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+@Table(name="StockPriceDetails",uniqueConstraints = {@UniqueConstraint(columnNames = "companyCode")})
+public class StockPriceDetails implements Serializable {
+
+	private static final long serialVersionUID = 3721936374836041464L;
+
+	// @GeneratedValue(strategy=GenerationType.AUTO)
+	// @javax.persistence.Id
+	private Long Id;
+	
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+	private Long companyCode;
+
+	@Column(precision=10, scale=2)
+	private Double currentStockPrice;
+	
+	@JsonFormat(pattern="yyyy-MM-dd")
+	private LocalDate stockPriceDate;
+	
+	@Column(columnDefinition = "TIME")
+	private LocalTime stockPriceTime;
+
+    @ManyToOne
+    private CompanyDetails company;
+
+	//---------------------------------------------------------------------------------------------------------------------------------
+	public StockPriceDetails() {
+		super();
+	}	
+	public StockPriceDetails(Long id, Long companyCode, Double currentStockPrice, LocalDate stockPriceDate,	LocalTime stockPriceTime) {
+		super();
+		this.Id = id;
+		this.companyCode = companyCode;
+		this.currentStockPrice = currentStockPrice;
+		this.stockPriceDate = stockPriceDate;
+		this.stockPriceTime = stockPriceTime;
+	}
+
+	// //---------------------------------------------------------------------------------------------------------------------------------
+	public Long getId() {
+		return Id;
+	}
+	public void setId(Long id) {
+		Id = id;
+	}
+	//---------------------------------------------------------------------------------------------------------------------------------
+	public Long getCompanyCode() {
+		return companyCode;
+	}
+	public void setCompanyCode(Long companyCode) {
+		this.companyCode = companyCode;
+	}
+	//---------------------------------------------------------------------------------------------------------------------------------
+	public Double getCurrentStockPrice() {
+		return currentStockPrice;
+	}
+	public void setCurrentStockPrice(Double currentStockPrice) {
+		this.currentStockPrice = currentStockPrice;
+	}
+	//---------------------------------------------------------------------------------------------------------------------------------
+	public LocalDate getStockPriceDate() {
+		return stockPriceDate;
+	}
+	public void setStockPriceDate(LocalDate stockPriceDate) {
+		this.stockPriceDate = stockPriceDate;
+	}
+	//---------------------------------------------------------------------------------------------------------------------------------
+	public LocalTime getStockPriceTime() {
+		return stockPriceTime;
+	}
+	public void setStockPriceTime(LocalTime stockPriceTime) {
+		this.stockPriceTime = stockPriceTime;
+	}
+}
